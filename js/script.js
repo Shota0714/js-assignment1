80921f59f6fe91b2c397fef5af070a58d71fb294
Shota0714 <u.shota0714@gmail.com>
2025-02-20 19:50:10 -0500

    add some comment

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+// Create element for a random story
 let randomStorySubject = ["The cat", "The bird", "The fish", "The dog"];
 let randomStoryAction = ["climbs", "sings", "swims", "barks"];
 let randomStoryObject = ["the tree", "a song", "in the lake", "in the garden"];
 
+// Get a random element 
 const story = document.getElementById("getRandomStory");
 story.addEventListener("click", function() {
+    // Output a random story
     let subject = randomStorySubject[Math.floor(Math.random() * randomStorySubject.length)];
     let action = randomStoryAction[Math.floor(Math.random() * randomStoryAction.length)];
     let object = randomStoryObject[Math.floor(Math.random() * randomStoryObject.length)];
